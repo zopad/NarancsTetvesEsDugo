35757210029094f9a063edee33a66246a2f8a55a
Zoboki Árpád <awd000@gmail.com>
2024-11-04 22:01:19 +0100

    Hoztam Füvet (basic setup)

---
+++ scripts/player_character.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+const SPEED = 420.0
+const JUMP_VELOCITY = -600.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
